5a34ff68b7aa0b075cb96a67e9aa12f17c9ae74d
Ali <ali.arioglu06@gmail.com>
2024-02-24 15:16:08 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+
+function buildStyles() {
+  return gulp
+    .src("sass/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(gulp.dest("css"));
+}
+
+function watchTask() {
+  gulp.watch(["sass/*.scss", "*.html"], buildStyles);
+}
+
+// function streamTask() {
+//   return gulp.src("*.js").pipe(gulp.dest("output"));
+// }
+
+// series -> task execute in order
+exports.default = gulp.series(buildStyles, watchTask);
+
+// parallel -> task execute in concurrency
+// exports.default = gulp.parallel(buildStyles, watchTask);
+
+// exports.default = streamTask;
